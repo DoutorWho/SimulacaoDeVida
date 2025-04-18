ba53385a2fce2641794253b73e775ffecc1cdf40
Elder <130578013+DoutorWho@users.noreply.github.com>
2025-04-18 11:37:15 -0300

    Update tarefas.h
    Atualizando pq o tarefas.c atualizou!

--- funcoes/tarefas.h
+++ funcoes/tarefas.h
@@ -9,6 +9,7 @@ void comprar(char bens[], float *dinheiro);
 
 void ver_banco(float saldo, char bens[]);
 
-int salvar_dados(char bens[], float *dinheiro, char cpf[]);
+int salvar_dados_banco(int numero_conta, char bens[], float *dinheiro, char cpf[]);
 
+int salvar_dados_bens(char bens[], char cpf[]);
 #endif
